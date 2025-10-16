b7582e882fc7536b1a73bea7862b9a4524705aaa
Davi-dev <rdricardodavi@gmail.com>
2025-10-16 20:50:50 +0000

    att

---
+++ menu.js
@@ -0,0 +1,18 @@
+const btnMenu = document.getElementById("hamburguer-button")
+const menu = document.getElementById("menu")
+
+btnMenu.classList.add("hamburguer-button-js-enabled")
+btnMenu.setAttribute("aria-expanded", "false")
+
+btnMenu.addEventListener("click", function(){
+
+        let expanded =this.getAttribute("aria-expanded")
+
+        if (expanded) {
+            menu.classList.add("menu-closed")
+        }else{
+            menu.classList.remove("menu-closed")
+        }
+
+        
+})
