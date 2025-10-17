075b445c466b563127d41df7ece16f8cfba2676d
Davi-dev <rdricardodavi@gmail.com>
2025-10-17 22:34:35 +0000

    att botaomenu

--- menu.js
+++ menu.js
@@ -3,16 +3,18 @@ const menu = document.getElementById("menu")
 
 btnMenu.classList.add("hamburguer-button-js-enabled")
 btnMenu.setAttribute("aria-expanded", "false")
+menu.setAttribute("aria-hidden", "true")
 
-btnMenu.addEventListener("click", function(){
+btnMenu.addEventListener("click", function() {
 
-        let expanded =this.getAttribute("aria-expanded")
+        let expanded = this.getAttribute("aria-expanded") === "true" ? true : false
 
         if (expanded) {
             menu.classList.add("menu-closed")
-        }else{
+        } else {
             menu.classList.remove("menu-closed")
         }
 
-        
+        this.setAttribute("aria-expanded",!expanded)
+        menu.setAttribute("aria-hidden", expanded)
 })
